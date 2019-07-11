e057932c0901b0138af938a7aa9875438afdec4f
hasagi <trichimtrich@gmail.com>
2019-07-11 11:03:21 +0800

    wip

--- udpserver.py
+++ udpserver.py
@@ -1,7 +1,7 @@
 import socket
 from hexdump import hexdump
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,5 +10,6 @@ print("Listen on {}:{}".format(UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
-    print("From [{}], message {} bytes:".format(addr, len(data)))
+    print("From [{}], message {} bytes: ... sendback".format(addr, len(data)))
     hexdump(data)
+    sock.sendto(data, addr)
